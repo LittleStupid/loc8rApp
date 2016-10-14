297a6056499133a98475e7ac47bd67e04761340c
LittleStupid <lttlstpd@live.com>
2016-10-14 21:27:14 +0800

    integrate with angularjs

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -48,17 +48,8 @@ var getLocationInfo = function ( req, res, callback ) {
   );
 };
 
-var renderHomepage = function( req, res, responseBody ) {
-  var message;
-  if ( !( responseBody instanceof Array )) {
-    message = "API lookup error";
-    responseBody = [];
-  } else {
-    if ( !responseBody.length ) {
-      message = "No places found nearby";
-    }
-  }
-  res.render( 'locations-list', {
+var renderHomepage = function(req, res){
+  res.render('locations-list', {
     title: 'Loc8r - find a place to work with wifi',
     pageHeader: {
       title: 'Loc8r',
@@ -66,11 +57,9 @@ var renderHomepage = function( req, res, responseBody ) {
     },
     sidebar: "Looking for wifi and a seat? Loc8r helps you find places to\
               work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r\
-              help you find the place you're looking for.",
-    locations: responseBody,
-    message: message
-  });
-}
+              help you find the place you're looking for."
+   });
+};
 
 var _formatDistance = function ( distance ) {
   var numDistance, unit;
@@ -85,31 +74,7 @@ var _formatDistance = function ( distance ) {
 }
 
 module.exports.homelist = function( req, res ) {
-  var requestOptions, path;
-  path = '/api/locations';
-  requestOptions = {
-    url: apiOptions.server + path,
-    method: "GET",
-    json: {},
-    qs: {
-      lng: -0.9630884,
-      lat: 51.451041,
-      maxDistance: 2000000000
-    }
-  };
-  request(
-    requestOptions,
-    function( err, response, body ) {
-      var i, data;
-      data = body;
-      if ( response.statusCode === 200 && data.length ) {
-        for ( i = 0; i < data.length; i++ ) {
-          data[i].distance = _formatDistance(data[i].distance);
-        }
-      }
-      renderHomepage( req, res, data );
-    }
-  );
+  renderHomepage( req, res );
 };
 
 var renderDetailPage = function(req, res, locDetail ) {
@@ -134,7 +99,8 @@ var renderReviewForm = function ( req, res, locDetail ) {
   res.render( 'location-review-form', {
     title: 'Review' + locDetail.name + 'on Loc8r',
     pageHeader: { title: 'Review ' + locDetail.name },
-    error: req.query.err
+    error: req.query.err,
+    url: req.originalUrl
   });
 };
 
